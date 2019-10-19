0edc9936cddf552e3f3c25aa398f2d94a7c4fdb2
anvcodes <kpeace0720@gmail.com>
2019-10-19 08:48:21 -0400

    minor adjustment

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -45,33 +45,5 @@
    var newBtn=  $("<button>").text(searchInput);
    $(".btns").append(newBtn);
   })
-           // for (var i = 0; i < output.length; i++) {
-  
-              // Only taking action if the photo has an appropriate rating
-              //if (output[i].rating !== "r" && results[i].rating !== "pg-13") {
-                // Creating a div for the gif
-               // var gifDiv = $("<div>");
-  
-                // Storing the result item's rating
-                //var rating = results[i].rating;
-  
-                // Creating a paragraph tag with the result item's rating
-               // var p = $("<p>").text("Rating: " + rating);
-  
-                // Creating an image tag
-               // var personImage = $("<img>");
-  
-                // Giving the image tag an src attribute of a proprty pulled off the
-                // result item
-               // personImage.attr("src", results[i].images.fixed_height.url);
-  
-                // Appending the paragraph and personImage we created to the "gifDiv" div we created
-               // gifDiv.append(p);
-               // gifDiv.append(personImage);
-  
-                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-              //  $("#gifs-appear-here").prepend(gifDiv);
-           // }
-         // }
-        
+          
     
